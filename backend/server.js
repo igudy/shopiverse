5e5fefa9937242a7d6357f9ef89d1995b02c519c
igudy <goodnessigunma1@gmail.com>
2023-11-08 23:30:57 +0100

    mongdb connected

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log("Hello Worldddd!!!");
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
