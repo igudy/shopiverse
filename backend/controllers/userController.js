af5714fb3833afda58cee58a2018f36b99c0e273
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-11-10 16:12:03 +0100

    feat: Register users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,60 @@
-const registerUser = async (req, res) => {
-  res.send("Register User");
-};
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const { generateToken } = require("../utils/index");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // validation
+  if (!name || !password || !email) {
+    res.status(400);
+    throw new Error("Please fill all the required fields");
+  }
+
+  if (password.length < 6 || password.length > 30) {
+    res.status(400);
+    throw new Error("Password must be more than 6 and less than 30 characters");
+  }
+
+  // Define user
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("Email already exist");
+  }
+
+  // Create new user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  // Generate Token
+  const token = generateToken(user._id);
+
+  // Send HTTP-only cookie
+  res.cookie("token", token, {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000 * 86400), //1Day
+    sameSite: "none",
+    secure: true,
+  });
+
+  if (user) {
+    const { _id, name, email, phone, bio, photo, role, isVerified } = user;
+
+    res
+      .status(201)
+      .json({ _id, name, email, phone, bio, photo, role, isVerified, token });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
 
 module.exports = {
   registerUser,
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -52,4 +52,4 @@ const userSchema = mongoose.Schema(
 );
 
 const User = mongoose.model("User", userSchema);
-model.exports = User;
+module.exports = User;
