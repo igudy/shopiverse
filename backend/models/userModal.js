b83afb0704aecb4618d8795ba381f02183e25bc7
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-11-10 13:54:07 +0100

    avatar icon added

---
+++ backend/models/userModal.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      trim: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please enter a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+    photo: {
+      type: String,
+      required: [true, "Please add a photo"],
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+    minimize: false,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+model.exports = User;
