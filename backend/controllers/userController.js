1b957c9b0412eae284df3f8b529d920e97166efc
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-11-12 16:24:31 +0100

    feat: getUser middleware and getUser controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -139,8 +139,30 @@ const logoutUser = asyncHandler(async (req, res) => {
   });
 });
 
+const getUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    const { _id, name, email, phone, bio, photo, role, isVerified } = user;
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      phone,
+      bio,
+      photo,
+      role,
+      isVerified,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
 module.exports = {
   registerUser,
   loginUser,
   logoutUser,
+  getUser,
 };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,11 +3,14 @@ const {
   registerUser,
   loginUser,
   logoutUser,
+  getUser,
 } = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logoutUser);
+router.get("/get-user", protect, getUser);
 
 module.exports = router;
