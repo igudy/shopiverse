bca4f9003e8e49055da6c555299d0974ef12ac06
igudy <goodnessigunma1@gmail.com>
2024-06-20 16:36:08 +0100

    feat: cardgraph test

--- frontend/src/components/admin/Card.tsx
+++ frontend/src/components/admin/Card.tsx
@@ -10,8 +10,11 @@ const Card = ({ top, icon: Icon, amount }: CardProps) => {
   return (
     <div>
       <div className="mt-4 text-sm flex bg-white rounded-3xl pl-2 pr-10 py-3 shadow-purple-200 shadow-md items-center">
-        <div className="bg-gray-200 rounded-full items-center m-2">
-          <Icon className="w-[40px] h-[40px] p-1 text-[#2B3674]" />
+        <div className="bg-gray-200 rounded-full items-center m-2" data-tes>
+          <Icon
+            className="w-[40px] h-[40px] p-1 text-[#2B3674]"
+            data-testid="card-icon"
+          />
         </div>
         <div className="flex-col space-y-1">
           <p className="text-[#39393b] text-[9px]">{top}</p>
--- frontend/src/components/admin/tests/Card.test.tsx
+++ frontend/src/components/admin/tests/Card.test.tsx
@@ -1,7 +1,45 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
+import Card, { CardGraph } from "../Card";
+import { FaBeer } from "react-icons/fa";
 
-describe("Simple Truthy Test Suite", () => {
-  it("should check if 1 is truthy", () => {
-    expect(1).toBeTruthy();
+describe("Card", () => {
+  // Because its a prop, you make sure the following are in the document.
+  it("should render the card component", () => {
+    render(<Card top={"Hello"} icon={FaBeer} amount={"1000"} />);
+    const top = screen.getByText("Hello");
+    const amount = screen.getByText("1000");
+    expect(top).toBeInTheDocument();
+    expect(amount).toBeInTheDocument();
+  });
+
+  // use data-testid for this one.
+  it("should render the svg(icon)", () => {
+    render(<Card top={"Hello"} icon={FaBeer} amount={"1000"} />);
+    const icon = screen.getByTestId("card-icon");
+    expect(icon).toBeInTheDocument();
+  });
+
+  // render cardGraph
+  it("should render card graph", () => {
+    render(<CardGraph>Card Graph Content</CardGraph>);
+    const text_content = screen.getByText("Card Graph Content");
+    expect(text_content).toBeInTheDocument();
+  });
+
+  // the first way of rendering
+  it("should render card graph", () => {
+    render(<CardGraph>Card Graph Content</CardGraph>);
+    const text_content = screen.getByText("Card Graph Content");
+    expect(text_content).toBeInTheDocument();
+  });
+
+  // second way of testing
+  it("should render card graph", () => {
+    const { getByText } = render(
+      <CardGraph>Testing the second style</CardGraph>
+    );
+    const textContent = getByText("Testing the second style");
+    expect(textContent).toBeInTheDocument();
   });
 });
