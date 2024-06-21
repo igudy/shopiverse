8981798475f13360d5554927ace278a99e945c9b
igudy <goodnessigunma1@gmail.com>
2024-06-21 21:32:21 +0100

    fix: status progress test

--- frontend/src/components/admin/StatusProgress.tsx
+++ frontend/src/components/admin/StatusProgress.tsx
@@ -7,7 +7,10 @@ interface IApproved {}
 export const Approved = ({}: IApproved) => {
   return (
     <div className="flex items-center font-bold text-[#2B3674]">
-      <FaCheck className=" bg-green-600 text-white w-5 h-5 rounded-full p-1 mr-1" />
+      <FaCheck
+        className="bg-green-600 text-white w-5 h-5 rounded-full p-1 mr-1"
+        aria-label="check_icon"
+      />
       Approved
     </div>
   );
@@ -16,7 +19,10 @@ export const Approved = ({}: IApproved) => {
 export const Disable = () => {
   return (
     <div className="flex items-center  font-bold text-[#2B3674]">
-      <IoCloseSharp className=" bg-red-600 text-white w-5 h-5 rounded-full p-1 mr-1" />
+      <IoCloseSharp
+        className=" bg-red-600 text-white w-5 h-5 rounded-full p-1 mr-1"
+        aria-label="close_sharp"
+      />
       Disable
     </div>
   );
@@ -25,7 +31,10 @@ export const Disable = () => {
 export const Error = () => {
   return (
     <div className="flex items-center  font-bold text-[#2B3674]">
-      <FaExclamation className=" bg-yellow-500 text-white w-5 h-5 rounded-full p-1 mr-1" />
+      <FaExclamation
+        className=" bg-yellow-500 text-white w-5 h-5 rounded-full p-1 mr-1"
+        aria-label="exclamation"
+      />
       Error
     </div>
   );
---
+++ frontend/src/components/admin/tests/StatusProgress.test.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Approved, Disable } from "../StatusProgress";
+
+describe("status progress", () => {
+  it("should render the approved component", () => {
+    render(<Approved />);
+    const approvedText = screen.getByText(/approved/i);
+    expect(approvedText).toBeInTheDocument();
+    const approvedIcon = screen.getByLabelText("check_icon");
+    expect(approvedIcon).toBeInTheDocument();
+  });
+
+  it("should render the disable component", () => {
+    render(<Disable />);
+    const disableText = screen.getByText(/disable/i);
+    expect(disableText).toBeInTheDocument();
+    const disableIcon = screen.getByLabelText("close_sharp");
+    expect(disableIcon).toBeInTheDocument();
+  });
+
+  it("should render the error component", () => {
+    render(<Disable />);
+    const errorText = screen.getByText(/error/i);
+    expect(errorText).toBeInTheDocument();
+    const errorIcon = screen.getByLabelText("exclamation");
+    expect(errorIcon).toBeInTheDocument();
+  });
+});
