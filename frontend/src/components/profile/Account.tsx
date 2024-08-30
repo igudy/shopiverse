78dc5cb5883798dedf53a2cd4c867e7e4829deec
igudy <goodnessigunma1@gmail.com>
2024-08-30 12:34:23 +0100

    feat: favicon added

--- frontend/src/components/profile/Account.tsx
+++ frontend/src/components/profile/Account.tsx
@@ -1,13 +1,42 @@
 import React from "react";
+import { CiDollar } from "react-icons/ci";
+import { FaCcMastercard } from "react-icons/fa";
+import { IoIosSend } from "react-icons/io";
+import Mastercard from '../../assets/mastercard.png';
+
 
 const Account = () => {
   return (
-    <div className="border-2 rounded-xl border-purple-500 shadow p-3">
+    <div className="border-2 rounded-xl border-purple-500 shadow-md p-3 h-[14rem] max-h-[14rem]">
       <div className="flex flex-col">
         <div>Hello,</div>
-        <div className="text-xl font-bold">Igudy</div>
-        <div className="w-full bg-gray-500 h-[0.3px]"></div>
-        <div className="text-xl font-bold">Igudy</div>
+        <div className="text-xl font-bold cursor-pointer">Igudy</div>
+
+        {/* Horizontal line */}
+        <div className="w-full bg-gray-500 h-[0.3px] mt-3"></div>
+        <div className="flex justify-between items-center">
+          <div>
+
+        <div className="text-xl font-medium mt-3">Account Balance</div>
+        <div className="text-md font-bold mt-2">$1000.00</div>
+
+          </div>
+          <div>
+            
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center my-2 gap-3">
+          <div className="flex gap-1 font-medium items-center p-2 bg-purple-700 text-white rounded-xl shadow-md hover:bg-purple-900 cursor-pointer w-[10rem] justify-center">
+            <CiDollar />
+            Deposit Money
+          </div>
+          <div className="flex gap-1 font-medium items-center p-2 bg-orange-500 text-white rounded-xl shadow-md hover:bg-orange-600 cursor-pointer w-[10rem] justify-center">
+            <IoIosSend />
+            Transfer
+          </div>
+
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/profile/WalletAds.tsx
+++ frontend/src/components/profile/WalletAds.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { AiFillGift } from "react-icons/ai";
+import { HiCurrencyDollar } from "react-icons/hi";
 
 const WalletAds = () => {
   return (
-    <div>WalletAds</div>
-  )
-}
+    <div className="border-2 rounded-xl border-purple-500 shadow-md p-3 h-[14rem] max-h-[14rem]">
+      <div className="flex">
+        <div className="w-[70%] flex flex-col justify-between">
+          <div className="font-medium text-xl gap-1 flex items-center"><HiCurrencyDollar /> Shopito Wallet</div>
+          <div className=" text-xl flex gap-1 items-center">Cashback up to 80% <AiFillGift /></div>
+          <div className="flex flex-wrap text-xs">Use your Shopiverse wallet at checkout and get up to 80% cashback </div>
+        </div>
+        <div className="w-30%">
+          <div>Ads Image</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default WalletAds
+export default WalletAds;
